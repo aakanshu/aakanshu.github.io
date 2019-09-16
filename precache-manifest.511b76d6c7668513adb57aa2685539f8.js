a91c66aa6e1d3f3ac9cd42b14c7fed6f1ff58e43
aakanshu <aakanshu@goodera.com>
2019-09-16 20:11:09 +0530

    Updates

---
+++ precache-manifest.511b76d6c7668513adb57aa2685539f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5723bb6f84ec6c47429ff3f430cc57b1",
+    "url": "/v2/index.html"
+  },
+  {
+    "revision": "8cbfe0ee48942d01dbb3",
+    "url": "/v2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "58bed043608efbeeea50",
+    "url": "/v2/static/js/2.bab60668.chunk.js"
+  },
+  {
+    "revision": "8cbfe0ee48942d01dbb3",
+    "url": "/v2/static/js/main.f948f6bf.chunk.js"
+  },
+  {
+    "revision": "a4c5fce8b2cd3a811835",
+    "url": "/v2/static/js/runtime~main.b09456cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
